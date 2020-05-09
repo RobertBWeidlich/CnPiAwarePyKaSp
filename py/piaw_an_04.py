e5d5114efed4009c530b842c5f69e7f389e2dae9
Robert Weidlich <RobertBWeidlich@yahoo.com>
2020-05-09 12:39:42 -0400

    Deal with three lists, This, Legacy, and Tracking

--- py/piaw_an_04.py
+++ py/piaw_an_04.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
-Loop to periodically poll the PiAware
-Retrieve data from PiAware via REST interface
-preliminary analysis - track all hex number, figure out the different records
+file:   piaw_an_04.py
+author: rbw
+date:   Sat May  9 12:34:49 EDT 2020
+purpose:
+  Loop to periodically poll the PiAware
+  Retrieve data from PiAware via REST interface
+  preliminary analysis - track all hex number, figure out the different records
+  Keep 2 lists:
+    1. This - currently polled flights
+    1. Legacy - all flights
+    2. Tracking - only those new flights discovered after start.
+         All flights in the This list not in Legacy and not in Tracking
+         are added to this list
 """
 import sys
 import requests
@@ -13,27 +24,52 @@ import time
 URL = 'http://192.168.1.208:8080/data/aircraft.json'
 POLL_PERIOD = 10     # seconds, must be integer > 0, should be 1,2,3,4,5,10,15,20,30, or 60
 WAIT_OFFSET = 0.0
-MAX_LOOP_COUNT = 4
+MAX_LOOP_COUNT = 10000000
 
+hex_list_legacy = None  # aircraft we found at start - don't analyze
+hex_list_tracking = []  # aircraft encountered after start
 
 """
 Analyze data, which is expected to be in JSON format
 """
 def analyze(json_data):
+    global analyze_count
+    global hex_list_legacy
+    global hex_list_tracking
     data = json.loads(json_data)
     #print(data)
-    d_now = data['now']
-    #print("d_now: %f" % d_now)
-    d_msg_count = data['messages']
-    #print('d_msg_count: %d' % d_msg_count)
-    d_aircraft_list = data['aircraft']
+    now = data['now']
+    #print("now: %f" % now)
+    msg_count = data['messages']
+    #print('msg_count: %d' % msg_count)
+    aircraft_list = data['aircraft']
+
     hex_list = []
-    for aircraft in d_aircraft_list:
+    for aircraft in aircraft_list:
         #print(aircraft)
         if 'hex' in aircraft:
             hex_list.append(aircraft['hex'])
     hex_list.sort()
+
+    if not hex_list_legacy:
+        hex_list_legacy = hex_list
+        return
+
+    # find new aircraft
+    for hex_s in hex_list:
+        if hex_s not in hex_list_legacy:
+            print("new: %s" % hex_s)
+            hex_list_legacy.append(hex_s)
+            hex_list_tracking.append(hex_s)
+
+    print("this    %d aircraft" % len(hex_list))
     print(hex_list)
+    print("legacy  %d aircraft" % len(hex_list_legacy))
+    print(hex_list_legacy)
+    print("tracking: %d aircraft" % len(hex_list_tracking))
+    print(hex_list_tracking)
+
+    print('')
 
 
 def main():
